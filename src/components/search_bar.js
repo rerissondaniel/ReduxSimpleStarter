05395239f21171b33dcc16cbb1de681a1cadd0de
Rerisson Daniel <rerissondaniel@gmail.com>
2018-09-08 14:41:35 -0300

    Styling with css

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,11 +9,10 @@ class SearchBar extends Component {
 
 	render () {
 		return (
-			<div>
+			<div className="search-bar">
 			<input 
 			value={this.state.term}
 			onChange={event => this.setState({term: event.target.value})}/>
-			Value of the input: {this.state.term}
 			</div>
 		);
 	}
