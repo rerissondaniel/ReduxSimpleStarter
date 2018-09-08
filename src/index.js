971f6af376c1be3cce16e9b8f6dfaf39a7dc9f8a
Rerisson Daniel <rerissondaniel@gmail.com>
2018-09-08 13:14:23 -0300

    Refactoring functional component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import SeachBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD6RHPcNkdGIRhY8PUBjW2myj33hUiRp8w';
 
-YTSeach({key: API_KEY, term: 'surfboards'}, (data) => {
-	console.log(data);
-});
-
 class App extends Component{
+
+	constructor(props){
+		super(props);
+		this.state = {videos: []};
+		YTSeach({key: API_KEY, term: 'surfboards'}, videos => {
+			this.setState({ videos });
+		});
+	}
+
 	render(){
 		return (
 			<div>
